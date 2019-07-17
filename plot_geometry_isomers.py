304b5a1e1a5cf01884432584317ffcfff3257fa7
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-07-17 10:17:08 +0100

    add cage only plot

--- plot_geometry_isomers.py
+++ plot_geometry_isomers.py
@@ -71,11 +71,12 @@ def make_plot(filename, energies):
     fig, ax = plt.subplots()
     X_positions = {
         'all': (2, '1111'), '1-down': (4, '1112'),
-        'cis': (6, '1122'), 'trans': (8, '1212')
+        'syn': (6, '1122'), 'anti': (8, '1212')
     }
     cs = ['r', 'b']
     ms = ['o', 'X']
-    solv = ['gas - xtb', 'DMSO - xtb']
+    # solv = ['gas - xtb', 'DMSO - xtb']
+    solv = ['DMSO - xtb']
     cages = ['cage1', 'cage2']
     for s, c in zip(solv, cs):
         for cage, m in zip(cages, ms):
@@ -88,7 +89,56 @@ def make_plot(filename, energies):
                 )
             new_Y_values = [i-min(Y_values) for i in Y_values]
             print(s, cage, new_Y_values)
-            label = f'{cage} - {s}'
+            if len(solv) > 1:
+                label = f'{cage} - {s}'
+            else:
+                label = f'{cage}'
+            ax.plot(
+                X_values, new_Y_values, c=c, label=label, marker=m
+            )
+
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel('isomer label', fontsize=16)
+    ax.set_ylabel('rel. total free energy [kJ/mol]', fontsize=16)
+    ax.set_xlim(0, 10)
+    ax.set_ylim(-5, 30)
+    ax.set_xticks([X_positions[i][0] for i in X_positions])
+    ax.set_xticklabels(list(X_positions.keys()))
+    ax.legend(fontsize=16)
+    fig.tight_layout()
+    fig.savefig(
+        filename,
+        dpi=720,
+        bbox_inches='tight'
+    )
+
+
+def make_cage_plot(filename, energies):
+    fig, ax = plt.subplots()
+    X_positions = {
+        'all': (2, '1111'), '1-down': (4, '1112'),
+        'syn': (6, '1122'), 'anti': (8, '1212')
+    }
+    cs = ['r', 'b']
+    ms = ['o', 'X']
+    # solv = ['gas - xtb', 'DMSO - xtb']
+    solv = ['DMSO - xtb']
+    cages = ['cage1', 'cage2']
+    for s in solv:
+        for cage, m, c in zip(cages, ms, cs):
+            X_values = []
+            Y_values = []
+            for X in X_positions:
+                X_values.append(X_positions[X][0])
+                Y_values.append(
+                    energies[cage][X_positions[X][1]][s] * 2625.50
+                )
+            new_Y_values = [i-min(Y_values) for i in Y_values]
+            print(s, cage, new_Y_values)
+            if len(solv) > 1:
+                label = f'{cage} - {s}'
+            else:
+                label = f'{cage}'
             ax.plot(
                 X_values, new_Y_values, c=c, label=label, marker=m
             )
@@ -118,8 +168,9 @@ def main():
 
     energies = energy_data()
 
-    filename = 'geometry_isomer_energies_xtb.pdf'
+    filename = 'geometry_isomer_energies_xtb_dmso.pdf'
     make_plot(filename, energies)
+    make_cage_plot(filename, energies)
 
 
 if __name__ == "__main__":
