6f406e4ba2a13ec72baa36139faf6d3af8fbf918
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-11-12 18:15:16 +0000

    add flexibility and output options

--- screening_process.py
+++ screening_process.py
@@ -12,6 +12,7 @@ Date Created: 05 Nov 2019
 """
 
 import sys
+import numpy as np
 from os.path import exists
 from itertools import combinations
 
@@ -74,7 +75,14 @@ def analyse_all_ligands(params, ligands):
         name = ligand
         molecule = ligands[ligand]
         if exists(f'{name}_opt_chosen.mol'):
+            with open(name+'_bites_dists.txt', 'r') as f:
+                lines = f.readlines()
+            bites_dist = [float(i.rstrip()) for i in lines]
+            with open(name+'_NNs_dists.txt', 'r') as f:
+                lines = f.readlines()
+            NN_dist = [float(i.rstrip()) for i in lines]
             molecule.update_from_file(f'{name}_opt_chosen.mol')
+
         else:
             print(f'doing {name}')
             # Get conformers.
@@ -83,8 +91,24 @@ def analyse_all_ligands(params, ligands):
                 N=int(params['N'])
             )
 
+            # Calculate and save their flexibility.
+            bites_dist = LA.calc_bite_flexibility(
+                molecule,
+                confs,
+                cids,
+                name
+            )
+
+            NN_dist = LA.calc_NN_flexibility(
+                molecule,
+                confs,
+                cids,
+                name
+            )
+
             # Plot their flexibility.
-            LA.plot_bite_flexibility(molecule, confs, cids, name)
+            bites_dist = LA.plot_bite_flexibility(bites_dist, name)
+            NN_dist = LA.plot_NN_flexibility(NN_dist, name)
 
             # Select conformer with binding groups pointing the right
             # way.
@@ -92,7 +116,7 @@ def analyse_all_ligands(params, ligands):
             molecule = LA.select_conformer(molecule, confs, cids, name)
             molecule.write(f'{name}_opt_chosen.mol')
             molecule.write(f'{name}_opt_chosen.xyz')
-        ligands[ligand] = molecule
+        ligands[ligand] = (molecule, bites_dist, NN_dist)
 
     return ligands
 
@@ -115,7 +139,7 @@ def build_all_cages(params, ligands):
     all_cage_sets = {}
     for ligand in ligands:
         name = ligand
-        molecule = ligands[ligand]
+        molecule = ligands[ligand][0]
         all_cage_sets[name] = CB.build_cage_isomers(
             name=name,
             ligand=molecule,
@@ -139,32 +163,100 @@ def analyse_all_cages(params, all_cage_sets, ligands):
 
     cages_cis_wins = []
     cages_not_wins = []
+    lig_studied = []
+    energy_preferences = []
+    plane_devs = []
 
-    for lig_name in all_cage_sets:
-        cages = all_cage_sets[lig_name]
-        energies = CA.get_cage_energies(lig_name, cages)
-        m_distortions = CA.get_metal_centre_distortion(lig_name, cages)
-        l_distortions = CA.get_ligand_distortion(lig_name, cages)
+    experiments = [
+        'li1_lk2_li5', 'li2_lk2_li6', 'li1_lk2_li4', 'li4_lk2_li5',
+    ]
 
-        print(energies)
-        print(l_distortions)
-        print(m_distortions)
+    with open('all_cage_results.txt', 'w') as f:
+        f.write(
+            f'lig,stable,preferred,plane_dev_C,'
+            'energy_A,energy_B,energy_C,energy_D\n'
+        )
+        for lig_name in all_cage_sets:
+            # if lig_name not in experiments:
+            #     continue
+            print('ligand:', lig_name)
+            lig_studied.append(lig_name)
+            cages = all_cage_sets[lig_name]
+            energies = CA.get_cage_energies(lig_name, cages)
+            m_distortions = CA.get_metal_centre_distortion(
+                lig_name,
+                cages
+            )
+            l_distortions = CA.get_ligand_distortion(
+                lig_name,
+                cages,
+                # bites_dist=ligands[lig_name][1],
+                # NN_dists=ligands[lig_name][2]
+                bites_dist=None,
+                NN_dists=None
+            )
 
-        cis_preferred_and_stable = all([
-            CA.check_stability(l_distortions, m_distortions),
-            CA.check_preference(energies, energy_cutoff=100)
-        ])
+            # print('bites', np.mean(ligands[lig_name][1]))
+            # print('bites', np.std(ligands[lig_name][1]))
+            # print('nn', np.mean(ligands[lig_name][2]))
+            # print('nn', np.std(ligands[lig_name][2]))
+            # for i in m_distortions['plane_dev'][0]:
+            #     print(i, m_distortions['plane_dev'][0][i])
+            #
+            # for i in l_distortions['bite_angle'][0]:
+            #     print(i, l_distortions['bite_angle'][0][i])
+            #
+            # for i in l_distortions['NN_dist'][0]:
+            #     print(i, l_distortions['NN_dist'][0][i])
+            # print('---')
 
-        if cis_preferred_and_stable:
-            cages_cis_wins.append(lig_name)
-        else:
-            cages_not_wins.append(lig_name)
+            stable = CA.check_stability(l_distortions, m_distortions)
+            preferred, energy_sep = CA.check_preference(
+                energies,
+                energy_cutoff=7.5
+            )
+            energy_preferences.append(energy_sep)
+            plane_devs.append(m_distortions['plane_dev'][0]['C'])
 
-    # Plot distribution of all cages.
-    total_cages = len(cages_cis_wins) + len(cages_not_wins)
-    print(
-        f'{len(cages_cis_wins)} cages with cis preffered '
-        f'and stable of {total_cages}.'
+            cis_preferred_and_stable = all([stable, preferred])
+            if cis_preferred_and_stable:
+                cages_cis_wins.append(lig_name)
+            else:
+                cages_not_wins.append(lig_name)
+
+            f.write(
+                f'{lig_name},{stable},{preferred},'
+                f"{m_distortions['plane_dev'][0]['C']},"
+                f"{energies['A']},{energies['B']},"
+                f"{energies['C']},{energies['D']}\n"
+            )
+
+        print('-----------------------------------------')
+        # Plot distribution of all cages.
+        total_cages = len(cages_cis_wins) + len(cages_not_wins)
+        print(
+            f'{len(cages_cis_wins)} cages with cis preffered '
+            f'and stable of {total_cages}.'
+        )
+        print('-----------------------------------------')
+        print('candidate cages:')
+        for i in cages_cis_wins:
+            print(i)
+
+    CA.plot_energetics(
+        lig_studied,
+        experiments,
+        cages_cis_wins,
+        cages_not_wins,
+        energy_preferences
+    )
+
+    CA.plot_plane_devs(
+        lig_studied,
+        experiments,
+        cages_cis_wins,
+        cages_not_wins,
+        plane_devs
     )
 
 
